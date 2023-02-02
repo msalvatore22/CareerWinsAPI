3fccbef9b3be6af6502c28036450d35835adb2f6
Michael Salvatore <msalvatore22@gmail.com>
2023-02-02 10:52:44 -0500

    Model the win class fields

---
+++ src/main/java/com/example/msalvatore22/CareerWinsAPI/Win.java
@@ -0,0 +1,25 @@
+package com.example.msalvatore22.CareerWinsAPI;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+import java.time.YearMonth;
+
+@Document(collection = "wins")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Win {
+    @Id
+    private ObjectId id;
+    private String title;
+    private String description;
+    private Boolean highlight;
+    private YearMonth yearMonth;
+    private LocalDate updatedAt;
+}
