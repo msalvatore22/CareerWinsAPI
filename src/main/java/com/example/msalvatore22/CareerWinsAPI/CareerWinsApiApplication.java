205964168c15fade64bd0cd541043c3af5ecc416
Michael Salvatore <msalvatore22@gmail.com>
2023-02-02 10:28:53 -0500

    First commit, set up mongo connection

---
+++ src/main/java/com/example/msalvatore22/CareerWinsAPI/CareerWinsApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.msalvatore22.CareerWinsAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerWinsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerWinsApiApplication.class, args);
+	}
+
+}
